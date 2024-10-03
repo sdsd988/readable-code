95ea02e49d23be98921da5228fda68b729a1dda6
JUNGSANGYUN <sdsd98987@gmail.com>
2024-10-03 20:56:30 +0900

    섹션4. 단일책임원칙

---
+++ src/main/java/cleancode/minesweeper/tobe/Minesweeper.java
@@ -0,0 +1,163 @@
+package cleancode.minesweeper.tobe;
+
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Minesweeper {
+
+    public static final int BOARD_ROW_SIZE = 8;
+    public static final int BOARD_COL_SIZE = 10;
+    private final GemaBoard gameBoard = new GemaBoard(BOARD_ROW_SIZE,BOARD_COL_SIZE);
+    private final ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
+    private final ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
+    private int gameStatus = 0; // 0: 게임 중, 1: 승리, -1: 패배
+
+    public void run() {
+
+        consoleOutputHandler.showGameStartComment();
+
+        gameBoard.initializeGame();
+
+        while (true) {
+            try {
+                consoleOutputHandler.showBoard(gameBoard);
+
+                if (doesUserWinTheGame()) {
+                    consoleOutputHandler.printGameWinningComment();
+                    break;
+                }
+                if (doesUserLoseTheGame()) {
+                    consoleOutputHandler.printGameLosingComment();
+                    break;
+                }
+
+                String cellInput = getCellInputFromUser();
+                String userActionInput = getUserActionInputFromUser();
+                actOnCell(cellInput, userActionInput);
+            } catch (AppException e) {
+                consoleOutputHandler.printExceptionMessage(e);
+            } catch (Exception e) {
+                consoleOutputHandler.printSimpleMessage("프로그램에 문제가 발생하였습니다.");
+            }
+        }
+    }
+
+
+    private void actOnCell(String cellInput, String userActionInput) {
+        int selectedColIndex = getSelectedColIndex(cellInput);
+        int selectedRowIndex = getSelectedRowIndex(cellInput);
+
+        if (doesUserChoseToPlantFlag(userActionInput)) {
+            gameBoard.flag(selectedRowIndex, selectedColIndex);
+            checkIfGameIsOver();
+            return;
+        }
+
+        if (doesUserChoseToOpenCell(userActionInput)) {
+            if (gameBoard.isLandMineCell(selectedRowIndex, selectedColIndex)) {
+                gameBoard.open(selectedRowIndex, selectedColIndex);
+                changeGameStatusToLose();
+                return;
+            }
+            gameBoard.openSurroundedCells(selectedRowIndex, selectedColIndex);
+            checkIfGameIsOver();
+            return;
+        }
+        throw new AppException("잘못된 번호를 선택하셨습니다.");
+
+    }
+
+    private void changeGameStatusToLose() {
+        gameStatus = -1;
+    }
+
+    private boolean doesUserChoseToOpenCell(String userActionInput) {
+        return userActionInput.equals("1");
+    }
+
+    private boolean doesUserChoseToPlantFlag(String userActionInput) {
+        return userActionInput.equals("2");
+    }
+
+    private int getSelectedRowIndex(String cellInput) {
+        char cellInputRow = cellInput.charAt(1);
+        return convertRowFrom(cellInputRow);
+    }
+
+    private int getSelectedColIndex(String cellInput) {
+        char cellInputCol = cellInput.charAt(0);
+        return convertColFrom(cellInputCol);
+    }
+
+    private String getUserActionInputFromUser() {
+        consoleOutputHandler.printCommentForUserAction();
+        return consoleInputHandler.getUserInput();
+    }
+
+    private String getCellInputFromUser() {
+        consoleOutputHandler.printCommentForSelectingCell();
+        return consoleInputHandler.getUserInput();
+    }
+
+    private boolean doesUserLoseTheGame() {
+        return gameStatus == -1;
+    }
+
+    private boolean doesUserWinTheGame() {
+        return gameStatus == 1;
+    }
+
+    private void checkIfGameIsOver() {
+        boolean isAllChecked = gameBoard.isAllCellChecked();
+        if (isAllChecked) {
+            changeGameStatusToWin();
+        }
+    }
+
+    private void changeGameStatusToWin() {
+        gameStatus = 1;
+    }
+
+    private int convertRowFrom(char cellInputRow) {
+        int rowIndex = Character.getNumericValue(cellInputRow) - 1;
+        if (rowIndex >= BOARD_ROW_SIZE) {
+            throw new AppException("잘못된 입력입니다.");
+        }
+        return rowIndex;
+    }
+
+    private int convertColFrom(char cellInputCol) {
+        switch (cellInputCol) {
+            case 'a':
+                return 0;
+            case 'b':
+                return 1;
+            case 'c':
+                return 2;
+            case 'd':
+                return 3;
+            case 'e':
+                return 4;
+            case 'f':
+                return 5;
+            case 'g':
+                return 6;
+            case 'h':
+                return 7;
+            case 'i':
+                return 8;
+            case 'j':
+                return 9;
+            default:
+                throw new AppException("잘못된 입력입니다.");
+        }
+    }
+
+
+
+
+}
